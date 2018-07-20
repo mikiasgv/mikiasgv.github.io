242d794cd7f6023bb420a50f30014b64089d7ed5
mikiasgv <mikiasgv@yahoo.com>
2018-07-20 15:25:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * Created by mikigv on 6/26/2018.
  */
-var staticCacheName = 'curr-conv-v20';
+var staticCacheName = 'curr-conv-v21';
 //comment
 self.addEventListener('install', (event) => {
     event.waitUntil(
